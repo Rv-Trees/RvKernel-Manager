6977f456aa30158ed1f8505f0b919fbc90dd0ba7
Rve27 <rve27github@gmail.com>
2024-10-09 22:53:11 +0000

    RvGpu: Set `loadMinGPUfreq()` to public
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/src/main/java/com/rvkernel/manager/RvGpu.java
+++ app/src/main/java/com/rvkernel/manager/RvGpu.java
@@ -91,7 +91,7 @@ public class RvGpu {
         }
     }
 
-    private int loadMinGPUfreq() {
+    public int loadMinGPUfreq() {
         try {
             Process process =
                     Runtime.getRuntime()
